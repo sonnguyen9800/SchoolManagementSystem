0a6f68ba172d8a4c611e7112fe5e7b96343670ca
SonNguyen9800 <sonnguyen9800@gmail.com>
2019-04-25 22:29:10 +1000

    First Commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   resources :locations
   resources :categories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+
 end
