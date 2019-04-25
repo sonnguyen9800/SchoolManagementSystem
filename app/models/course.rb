21ffe2f4fa3e34df8c4afbe4c5e3e15f7e0f133f
SonNguyen9800 <sonnguyen9800@gmail.com>
2019-04-25 17:35:47 +1000

    Finish Course and Category

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,6 @@
 class Course < ApplicationRecord
   has_and_belongs_to_many :locations
-  has_and_belongs_to_many :categories
+  has_and_belongs_to_many :categories, :join_table => 'courses_categories'
 
 
 end
