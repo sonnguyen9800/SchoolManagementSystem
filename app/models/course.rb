1ba5c8841ea0b62f94c6f6c926e8d28f0fdd1fb5
SonNguyen9800 <sonnguyen9800@gmail.com>
2019-04-28 16:11:16 +1000

    Some fix

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ApplicationRecord
   has_and_belongs_to_many :locations
   has_and_belongs_to_many :categories, :join_table => 'courses_categories'
-  belongs_to :coordinator, :foreign_key => 'coordinator_id'
+  belongs_to :coordinator, :foreign_key => 'coordinator_id', optional: true
 end
