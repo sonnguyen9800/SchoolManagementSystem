05b48f1047ad8696fb8aebde1f3c743fb7a905f3
SonNguyen9800 <sonnguyen9800@gmail.com>
2019-05-17 23:18:42 +1000

    Finish CR

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_17_115931) do
+ActiveRecord::Schema.define(version: 2019_05_17_131621) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -56,7 +56,7 @@ ActiveRecord::Schema.define(version: 2019_05_17_115931) do
     t.integer "coordinator_id"
     t.integer "like"
     t.integer "dislike"
-    t.string "description"
+    t.text "description"
   end
 
   create_table "courses_categories", id: false, force: :cascade do |t|
