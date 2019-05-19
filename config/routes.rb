be21ae092be20fb4bca0cf928e9cd7b36b9d2ed1
quocanhdk51 <quocanhdk51@gmail.com>
2019-05-19 03:22:20 +0000

    email

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :contacts
   get 'downvotes/create'
   get 'upvotes/create'
   get 'upvotes/destroy'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_17_131621) do
+ActiveRecord::Schema.define(version: 2019_05_18_074830) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -39,6 +39,14 @@ ActiveRecord::Schema.define(version: 2019_05_17_131621) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "coordinators", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -51,12 +59,12 @@ ActiveRecord::Schema.define(version: 2019_05_17_131621) do
   create_table "courses", force: :cascade do |t|
     t.string "name"
     t.string "prerequisite"
+    t.integer "coordinator_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "coordinator_id"
     t.integer "like"
     t.integer "dislike"
-    t.text "description"
+    t.string "description"
   end
 
   create_table "courses_categories", id: false, force: :cascade do |t|
@@ -82,14 +90,6 @@ ActiveRecord::Schema.define(version: 2019_05_17_131621) do
 
   create_table "locations", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "messages", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
