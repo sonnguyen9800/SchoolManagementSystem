7a059d9c8628d6c8b78dd0829f8f9a219ed35176
SonNguyen9800 <sonnguyen9800@gmail.com>
2019-05-20 22:58:04 +1000

    Quick Fix

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,7 +2,6 @@ class MainController < ApplicationController
   #before_action :logged_in_coordinator, only: [:home, :help]
   def home
     if logged_in?
-      flash[:success] = "Welcome Back"
       redirect_to courses_path
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_19_090101) do
+ActiveRecord::Schema.define(version: 2019_05_20_110204) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2019_05_19_090101) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "adminflag"
   end
 
   create_table "courses", force: :cascade do |t|
